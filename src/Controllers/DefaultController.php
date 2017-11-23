8d020116607e463ea46c96283c432ccc3848f31b
Pandora <pandoraangora@gmail.com>
2017-11-24 00:51:41 +0100

    admin_contact page

--- src/Controllers/DefaultController.php
+++ src/Controllers/DefaultController.php
@@ -10,51 +10,82 @@ use Weasley\Model\Repository\UserManager;
  */
 class DefaultController extends Controller
 {
-	/**
-	 * Render index
-	 */
-	public function indexAction(){
-		return $this->twig->render('user/home.html.twig');
-	}
+    /**
+     * Render index
+     */
+    public function indexAction()
+    {
+        return $this->twig->render('user/home.html.twig');
+    }
 
-	/**
-	 * Render concept
-	 */
-	public function conceptAction(){
+    /**
+     * Render concept
+     */
+    public function conceptAction()
+    {
         return $this->twig->render('user/concept.html.twig');
-	}
+    }
+
     /**
      * Render product
      */
-    public function produitsAction(){
+    public function produitsAction()
+    {
         return $this->twig->render('user/produits.html.twig');
     }
 
     /**
      * Render contact
      */
-	public function contactAction(){
+    public function contactAction()
+    {
         return $this->twig->render('user/contact.html.twig');
-	}
+    }
 
     /**
      * Render login
      */
-	public function loginAction(){
-	    return $this->twig->render('admin/login.html.twig');
+    public function loginAction()
+    {
+        return $this->twig->render('admin/login.html.twig');
     }
 
     /**
      * Render admin
      */
-    public function adminAction(){
+    public function adminAction()
+    {
         return $this->twig->render('admin/admin.html.twig');
     }
 
     /**
      * Render admin contact
      */
-    public function adminContactAction(){
-        return $this->twig->render('admin/admin_contact.html.twig');
+    public function adminContactAction()
+    {
+        return $this->twig->render('admin/admin_contact.html.twig');/*
+        if ($_SERVER['REQUEST_METHOD'] == "POST") {
+            $errors = [];
+            foreach ($_POST as $key => $value) {
+                if (empty($_POST[$key])) {
+                    $errors[$key] = "Veuillez renseigner le champ " . $key;
+                }
+            }
+
+            if (!empty($errors)) {
+                return $this->twig->render('admin/admin_contact.html.twig', array(
+                    'errors' => $errors
+                ));
+
+            } else {
+                $adresse = $_POST['adresse'];
+                $horaire = $_POST['horaire'];
+                $numero = $_POST['numero'];
+                $commentaire = $_POST['commentaire'];
+                $manager = new ModelManager();
+                $manager->updateContact($adresse, $horaire, $numero, $commentaire);
+            }
+            return $this->twig->render('admin/admin_contact.html.twig');
+        }*/
     }
 }
