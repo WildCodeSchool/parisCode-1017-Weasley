8408486cb98fdf11e9868413773981804343fccc
Valériane Aron <valeriane.aron@gmail.com>
2017-11-28 14:35:51 +0100

    update of pack explosion.jpg

--- app/config.php
+++ app/config.php
@@ -1,7 +1,7 @@
 <?php
 
-define('APP_DB_NAME', 'Weasley');
+define('APP_DB_NAME', 'weasley');
 define('APP_DB_USER', 'root');
-define('APP_DB_PWD', 'pandora1987');
+define('APP_DB_PWD', 'root');
 define('APP_DB_HOST', 'localhost');
-define('APP_DB_PORT', '3306');
+define('APP_DB_PORT', '8889');
