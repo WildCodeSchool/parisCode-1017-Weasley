092caa5a4cb716d527bd36f2f68ba14d621f5300
Valériane Aron <valeriane.aron@gmail.com>
2017-11-28 15:17:00 +0100

    creation of adminController, contactController, ProductsController + index references

--- public/index.php
+++ public/index.php
@@ -4,9 +4,18 @@
 require_once '../vendor/autoload.php';
 require_once '../app/config.php';
 
+// Rappel des controllers
+
 use Weasley\Controllers\DefaultController;
+use Weasley\Controllers\AdminController;
+use Weasley\Controllers\ContactController;
+use Weasley\Controllers\ProductsController;
 
 $defaultController = new DefaultController();
+$adminController = new AdminController();
+$contactController = new ContactController();
+$productsController = new ProductsController();
+
 
 if (empty($_GET)){
 	echo $defaultController->indexAction();
@@ -15,19 +24,19 @@ if (empty($_GET)){
 	echo $defaultController->conceptAction();
 
 } elseif ($_GET['section'] == "produits"){
-    echo $defaultController->produitsAction();
+    echo $productsController->produitsAction();
 
 } elseif ($_GET['section'] == "contact"){
-	echo $defaultController->contactAction();
+	echo $contactController->contactAction();
 
 } elseif ($_GET['section']== "login"){
-    echo $defaultController->loginAction();
+    echo $adminController->loginAction();
 
 } elseif ($_GET['section']== "admin"){
-    echo $defaultController->adminAction();
+    echo $adminController->adminAction();
 
 } elseif ($_GET['section']== "admin_contact") {
-    echo $defaultController->adminContactAction();
+    echo $adminController->adminContactAction();
 }
 
 //$adminController !!!
---
+++ src/Controllers/AdminController.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: valerianearon
+ * Date: 28/11/2017
+ * Time: 14:52
+ */
+
+namespace Weasley\Controllers;
+
+use Weasley\Model\Repository\UserManager;
+
+class AdminController extends Controller
+
+{ /**
+ * Render login
+ */
+    public function loginAction()
+    {
+        return $this->twig->render('admin/login.html.twig');
+    }
+
+    /**
+     * Render admin
+     */
+    public function adminAction()
+    {
+        return $this->twig->render('admin/admin.html.twig');
+    }
+
+    /**
+     * Render admin contact
+     */
+    public function adminContactAction()
+    {
+        return $this->twig->render('admin/admin_contact.html.twig');/*
+        if ($_SERVER['REQUEST_METHOD'] == "POST") {
+            $errors = [];
+            foreach ($_POST as $key => $value) {
+                if (empty($_POST[$key])) {
+                    $errors[$key] = "Veuillez renseigner le champ " . $key;
+                }
+            }
+
+            if (!empty($errors)) {
+                return $this->twig->render('admin/admin_contact.html.twig', array(
+                    'errors' => $errors
+                ));
+
+            } else {
+                $adresse = $_POST['adresse'];
+                $horaire = $_POST['horaire'];
+                $numero = $_POST['numero'];
+                $commentaire = $_POST['commentaire'];
+                $manager = new ModelManager();
+                $manager->updateContact($adresse, $horaire, $numero, $commentaire);
+            }
+            return $this->twig->render('admin/admin_contact.html.twig');
+        }*/
+    }
+
+}
