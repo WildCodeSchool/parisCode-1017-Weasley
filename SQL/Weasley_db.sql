29bef79d5ef53777f8f1b63b418e66f666a185ab
Elisa <elisa.laristan@gmail.com>
2017-11-29 11:57:14 +0100

    grow the database weasley for the section products

--- SQL/Weasley_db.sql
+++ SQL/Weasley_db.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 5.6.35, for osx10.9 (x86_64)
 --
--- Host: localhost    Database: weasley
+-- Host: localhost    Database: Weasley
 -- ------------------------------------------------------
 -- Server version	5.6.35
 
@@ -16,14 +16,6 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Current Database: `weasley`
---
-
-CREATE DATABASE /*!32312 IF NOT EXISTS*/ `weasley` /*!40100 DEFAULT CHARACTER SET utf8 */;
-
-USE `weasley`;
-
---
 -- Table structure for table `contact`
 --
 
@@ -37,7 +29,7 @@ CREATE TABLE `contact` (
   `ouverture` varchar(250) DEFAULT NULL,
   `commentaire` varchar(250) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -62,8 +54,9 @@ CREATE TABLE `produits` (
   `nom_produit` varchar(100) DEFAULT NULL,
   `description_produit` text,
   `image_url` varchar(255) DEFAULT NULL,
+  `cat_produits` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id_produit`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -72,6 +65,7 @@ CREATE TABLE `produits` (
 
 LOCK TABLES `produits` WRITE;
 /*!40000 ALTER TABLE `produits` DISABLE KEYS */;
+INSERT INTO `produits` VALUES (1,'Boîte à flemme','Les boîtes à Flemme sont une invention de Fred et George Weasley. Il s\'agit d\'un choix de confiseries qui rendent malade.','assets/pictures/produits/boite_flemme.jpg','Friandises magiques'),(3,'Nougat','Une des sortes de sucreries que l\'on trouve dans une boîte à Flemme. Chaque bout du bonbon est d\'une couleur différente, l\'un provoque une fort saignement de nez et l\'autre annule ses effets.','assets/pictures/produits/nougat.jpg','Friandises Magiques'),(4,'Pralines','Pralines Longue Langue: inventées par les jumeaux Weasley, ces pralines allongent considérablement la taille de la langue de la victime qui les avalent.','assets/pictures/produits/pralines.jpg','Friandises Magiques'),(5,'Dragées','Une des sortes de sucreries que l\'on trouve dans une boîte à Flemme. Chaque bout du bonbon est d\'une couleur différente, l\'un provoque un vomissement et l\'autre annule ses effets. Fred et George ont eu des problèmes avec le prototype jusqu\'à ce qu\'ils réussissent équilibrer les effets pour que le vomissement soit important mais pas trop afin de pouvoir tout de même avaler le remède.','assets/pictures/produits/dragees.jpg','Friandises magiques'),(6,'Ballons','Ballons lumineux increvables: Etranges ballons magiques à gonfler afin de créer une diversion très bruyante lors d\'une situation épineuse. Prix : 1 gallion l\'unité et 5 gallions la boite de 10 ballons . Encore en stock .','assets/pictures/produits/boite_flemme.jpg','Farces et artifices'),(7,'Bomba-bouse','Bombabouse: Quand on les jette, elles dégagent une odeur putride. Quand on les tient, elles salissent les mains . Prix : 1 gallion l\'unité et 5 gallions la boite de 10 bombabouse . Encore en stock .','assets/pictures/produits/nougat.jpg','Farces et artifices'),(8,'Boule Puante','Boules puantes: Objets minuscules, très appréciés de Fred et George Weasley, qui créent un nuage de fumée verte nauséabond qui peut semer le trouble auprès d\'adversaires ou de préfets. Prix : 1 gallion l\'unité et 5 gallions la boite de 10 boules puantes . Encore en stock .','assets/pictures/produits/pralines.jpg','Farces et artifices'),(9,'Déflagration Deluxe','Déflagration Deluxe : Sélection de luxe des Feuxfous Buseboum, les feux d\'artifices des jumeaux. Prix : 20 gallions l\'unité . Ne se vend pas en lot . Encore en Stock .\r\n','assets/pictures/produits/dragees.jpg','Farces et artifices'),(10,'Feux-fous Fuse-boum','Feuxfous Fuseboum :Feux d\'artifices magiques provoquant différents effets visuels, grâce à des enchantements qui créent des variations si deux fusées se rentrent dedans ou si certains sortilèges sont utilisés pour tenter de s\'en débarrasser. Essayer de les pétrifier résulte en une explosion, par exemple, tandis qu\'un sortilège de Disparition multiplie par 10 l\'effet du feu d\'artifice visé. Prix : 2 gallions l\'unité et 20 gallions la boite de 10 feuxfous fuseboum . Encore en stock .\r\n','assets/pictures/produits/boite_flemme.jpg','Farces et artifices'),(11,'Flambée de base','Flambée de Base : Sélection de base des Feuxfous Buseboum, les feux d\'artifices des jumeaux. Prix : 5 gallions l\'unité . Ne se vends pas en lot . Encore en stock .\r\n','assets/pictures/produits/nougat.jpg','Farces et artifices'),(12,'Marécage portable','Marécage Portable : Lorsqu\'il est déployé, il crée un marécage sur la zone ciblée. C\'est une Farce qui résulta de la fameuse retenue ratée d\'Ombrage. Prix : 5 gallions l\'unité . Ne se vends pas en lot . Encore en stock .\r\n','assets/pictures/produits/dragees.jpg','Farces et artifices'),(13,'Pétards','Pétards moulliés du Dr Flibuste: Pétards qui explosent sans faire de chaleur . Prix : 10 mornilles l\'unité et 1 gallion la boite de 10 ballons . Encore en stock .\r\n','assets/pictures/produits/dragees.jpg','Farces et artifices'),(14,'Chapeau sans tête','Chapeau sans tête: il permet de faire disparaître le chapeau et la tête en même temps, ce qui est vraiment impressionnant d\'après Hermione! Le ministère en a en effet commandé pour ses missions contre Voldemort, car ce chapeau peut être d\'une grande aide ! Prix : 6 gallions l\'unité et 45 gallions la boite de 10 chapeaux sans têtes . Encore en stock .','assets/pictures/produits/boite_flemme.jpg','Accessoires'),(15,'Crèmes Canaries','Crèmes canaries: la personne sans s\'en rendre en compte se transforme en un grand canari géant pendant quelques secondes. Prix :10 mornilles l\'unité et 1 gallion la boite de 10 crèmes canaries . Encore en stock .\r\n','assets/pictures/produits/nougat.jpg','Accessoires'),(16,'Fausses baguettes','Fausses baguettes : Ces objets ressemblent à des baguettes normales mais quand on essaie de les utiliser, ils se changent en quelque chose d\'amusant comme un perroquet en fer blanc ou un haddock en caoutchouc (pour la version la moins chère), ou frappera l\'utilisateur imprudent sur la tête (la version la plus chère). Prix : 5 gallions l\'unité . Ne se vends pas en lot . Encore en stock .','assets/pictures/produits/pralines.jpg','Accessoires'),(17,'Oreille extensible','Oreilles à rallonges: Longue ficelle couleur chair qui permet à celui qui l\'introduit dans son oreille d\'ecouter des conversations. Inventé par les jumeaux Weasley, vous pourrez en trouver dans leur boutique farces pour sorciers facetieux. Prix : 3 gallions l\'unité et 10 gallions la boite de 10 oreilles à rallonges . Encore en stock . AFFAIRE DE LA BOUTIQUE : 10 GALLIONS LA BOITE AU LIEU DE 30 GALLIONS !!','assets/pictures/produits/dragees.jpg','Accessoires'),(18,'Disparition','Le pack disparition vous permet de vous éclipser instantanément du moins, aux yeux de tous. Vous pourrez ainsi être témoins de scènes incroyables sans être vu. ','assets/pictures/produits/pack_disparition_.jpg','Packs Magiques'),(19,'Pack explosion','Les packs explosions: envie de sensations fortes? Besoin de pimenter votre quotidien de Moldu? Le pack explosion est fait pour vous. Que ce soit pour surprendre vos collègues, pour éloigner vos ennemis ou pour boucler en beauté une dispute avec votre moitié, n\'hésitez plus: faites exploser !!! ','assets/pictures/produits/pack_explosion.jpg','Packs magiques');
 /*!40000 ALTER TABLE `produits` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -108,4 +102,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-11-28 16:41:24
+-- Dump completed on 2017-11-29 11:55:53
